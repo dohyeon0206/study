e79017a7a269cbf0a8e85282007ae1b51dfca277
dohyeon <lshdh060206@gmail.com>
2022-12-20 12:22:51 +0900

    commit

---
+++ 소스.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int num1, num2, a, Panswer, Canswer, count = 0, i = 1;
+	double k;
+	double c;
+
+	while (i < 10) {
+		num1 = rand() % 100 + 1;
+		num2 = rand() % num1 + 1;
+		a = rand() % 4;
+
+		
+	}
+}
