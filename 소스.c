f6b9b10a3ec034256369905f79b111de03346658
dohyeon <lshdh060206@gmail.com>
2022-12-20 12:25:34 +0900

    scond commit

--- 소스.c
+++ 소스.c
@@ -11,6 +11,26 @@ int main(void) {
 		num2 = rand() % num1 + 1;
 		a = rand() % 4;
 
+		if (a == 0) {
+			printf("[%d/10] %d + %d = ", i, num1, num2);
+			scanf_s("%d", &Panswer);
+			Canswer = num1 + num2;
+			if (Panswer == Canswer) {
+				i++;
+				printf("정답입니다.\n");
+			}
+			else printf("오답입니다. 정답은 %d입니다.\n", Canswer);
+		}
+		else if (a == 1) {
+			printf("[%d/10] %d - %d = ", i, num1, num2);
+			scanf_s("%d", &Panswer);
+			Canswer = num1 - num2;
+			if (Panswer == Canswer) {
+				i++;
+				printf("정답입니다.\n");
+			}
+			else printf("오답입니다. 정답은 %d입니다.\n", Canswer);
+		}
 		
 	}
 }
